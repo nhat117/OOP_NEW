e51dbc605e235ae4faa6da18ffbb836fd15dc5e9
Nhat Bui <s3878174@rmit.edu.vn>
2021-06-05 17:38:05 +0700

    Complete Wk4 Ex 4 & 5

--- src/WK3/Intersect.java
+++ src/WK3/Intersect.java
@@ -65,7 +65,7 @@ public class Intersect {
 
     public static void main(String[] args) {
         Line l1 = new Line(1,2,3,4);
-        Line l2 = new Line(1,2,3,4);
+        Line l2 = new Line(1,4,3,4);
         l1.intersectpoint(l2);
 
     }
